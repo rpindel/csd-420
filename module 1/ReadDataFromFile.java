e7164620018452e49d5afc69d247ccef933a4f22
Robin Pindel <robin.pindel@gmail.com>
2023-03-19 10:07:17 -0500

    Final code work for mod 1 assignment

--- module 1/ReadDataFromFile.java
+++ module 1/ReadDataFromFile.java
@@ -11,25 +11,30 @@
 public class ReadDataFromFile {
     //Main method
     public static void main (String[] args) throws ClassNotFoundException, IOException {
+        
+        //Create input stream and read all created objects from data file pindelDataFile.dat
         try (
             ObjectInputStream inputRead = new ObjectInputStream(new FileInputStream("pindelDataFile.dat"));
         ) {
-            //int[] intRandomArray = new int[5];
-            //double[] doubleRandomArray = new double[5];
-            
+            //Create variables to assign read object data to for console display    
             int[] intRandomArray = (int[])(inputRead.readObject());
             Date todaysDate = (java.util.Date)(inputRead.readObject());
             double[] doubleRandomArray = (double[])(inputRead.readObject());
 
+            //Console display of random integer array
+            System.out.println("");
             for (int i = 0; i < intRandomArray.length; i++) {
             System.out.print(intRandomArray[i] + " ");
             }
 
+            //Console display of date object content
             System.out.println("\n" + todaysDate);
 
+            //Console display of random double array
             for (int i = 0; i < doubleRandomArray.length; i++) {
             System.out.print(doubleRandomArray[i] + " ");
             }
+            System.out.println("\n");
         }
     }
 }
--- module 1/WriteDataToFile.java
+++ module 1/WriteDataToFile.java
@@ -2,7 +2,7 @@
  * Module 1 Data Work Programs Assignment
  * 3/14/2023
  * Write Data To File Program
- * This program writes data to a file pindelDataFile.dat
+ * This program writes data to the file pindelDataFile.dat
  */
 
 import java.io.*;
@@ -11,24 +11,22 @@ import java.util.*;
 public class WriteDataToFile {
     //Main method
     public static void main (String[] args) throws IOException {
+        //Create array of 5 random integers between 1 and 10
         int[] intRandomArray = new int[5];
         for (int i = 0; i < 5; i++) {
             intRandomArray[i] = (int)(Math.random() * 10) + 1;          
         }
-        for (int i = 0; i < intRandomArray.length; i++) {
-            System.out.print(intRandomArray[i] + " ");
-        }
 
+        //Create an instance of date for today's date
         Date todaysDate = new java.util.Date();
-        System.out.println("\n" + todaysDate);
         
+        //Create array of 5 random doubles between 1 and 10
         double[] doubleRandomArray = new double[5];
         for (int i = 0; i < 5; i++) {
             doubleRandomArray[i] = (double)(Math.random() * 10) + 1;
         }
-        for (int i = 0; i < doubleRandomArray.length; i++) {
-            System.out.print(doubleRandomArray[i] + " ");
-        }
+
+        //Create output stream and write all created objects to data file pindelDataFile.dat
         try (
             ObjectOutputStream outputWrite = new ObjectOutputStream(new FileOutputStream("pindelDataFile.dat"));
         ) {
