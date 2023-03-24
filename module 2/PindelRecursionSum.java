e77e54eb175793b533669aed0e9daec116d9cb56
tk78876 <Robin.Pindel@kohls.com>
2023-03-24 12:33:58 -0500

    Good amount of work for mod 2 assignment

--- module 2/PindelRecursionSum.java
+++ module 2/PindelRecursionSum.java
@@ -7,24 +7,34 @@ import java.util.*;
 
 public class PindelRecursionSum {
     //main method
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InputMismatchException {
         //Create scanner for user input
         Scanner input = new Scanner(System.in);
-        System.out.println("Please enter an integer value for n greater than 1: ");
-        int n = input.nextInt();
-        System.out.println("The sum of your fractions in double form is " + sum(n));
+        System.out.println("Let's calculate the sum of fractions from i / i + i down through 1 / 2.");
+        System.out.println("Please enter an integer value for i greater than 0: ");
+        
+        try {
+            double i = input.nextInt();
+            System.out.println("The sum of your fractions in double form is " + sum(i));
+        }
+        catch(Exception e) {
+            System.out.println("<< Invalid value type entered >>");
+            System.out.println("Please re-run the program and enter an integer value greated than zero.");
+            System.exit(1);
+        }    
         input.close();
     }
 
     //Recursive method
-    public static double sum(int n) {
-        double sum = 0;
-        if (n < 2) {
-            return sum + (n / n + 1);
+    public static double sum(double i) {
+        if (i > 1) {
+            //System.out.println("Rec n = " + i);
+            return (i / (i + 1)) + sum(i - 1);
         }
         else {
-            sum += (n / n + 1);
-            return sum(n - 1);
+            //System.out.println("Base n = " + i);
+            return (i / (i + 1));
         }
     }
-}
+}   
+
