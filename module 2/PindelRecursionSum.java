0ef91aa2c8b54c271d44f75ab0ed7ce8ba0b9815
Robin Pindel <robin.pindel@gmail.com>
2023-03-24 21:15:27 -0500

    Final code clean up for mod 2 assignment

--- module 2/PindelRecursionSum.java
+++ module 2/PindelRecursionSum.java
@@ -9,7 +9,7 @@
 import java.util.*;
 
 public class PindelRecursionSum {
-    //main method
+    //Main method
     public static void main(String[] args) throws InputMismatchException {
         //Create scanner for user input
         Scanner input = new Scanner(System.in);
