8a843ce9962882b245a264290c1f7418565f4577
Robin Pindel <robin.pindel@gmail.com>
2023-03-24 21:07:06 -0500

    Code clean up

--- module 2/PindelRecursionSum.java
+++ module 2/PindelRecursionSum.java
@@ -1,6 +1,9 @@
 /*Robin Pindel
  * Module 3 Recursin Sum Assignment
  * 3/24/2023
+ * 
+ * This application takes user input and recursively calculates the sum of 
+ * fractions based on the user input.
  */
 
 import java.util.*;
@@ -11,28 +14,33 @@ public class PindelRecursionSum {
         //Create scanner for user input
         Scanner input = new Scanner(System.in);
         System.out.println("");
-        System.out.println("Let's calculate the sum of fractions from i / i + i down through 1 / 2.");
+        System.out.println("Let's calculate the sum of fractions from i / (i + "
+        + "1) down through 1 / 2.");
         System.out.println("Please enter an integer value for i greater than 0: ");
         
         try {
-            double i = input.nextInt();
+            int i = input.nextInt();
             System.out.println("");
-            System.out.println("\nThe sum of your fractions in double form is " + sum(i) + ".");
+            System.out.println("\nThe sum of your fractions in double form is " 
+            + fractionSum(i) + ".");
             System.out.println("");
         }
         catch(Exception e) {
-            System.out.println("<< Invalid value type entered >>");
-            System.out.println("Please re-run the program and enter an integer value greated than zero.");
+            System.out.println("");
+            System.out.println("<< Exception - Invalid value type entered >>");
+            System.out.println("Please re-run the program and enter an integer "
+            + "value greated than zero.");
+            System.out.println("");
             System.exit(1);
         }    
         input.close();
     }
 
     //Recursive method
-    public static double sum(double i) {
+    public static double fractionSum(double i) {
         if (i > 1) {
             System.out.println((int)i + " / " + (int)(i + 1) + " + ");
-            return (i / (i + 1)) + sum(i - 1);
+            return (i / (i + 1)) + fractionSum(i - 1);
         }
         else {
             System.out.println((int)i + " / " + (int)(i + 1) + " =");
