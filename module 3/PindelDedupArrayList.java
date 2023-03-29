5dd30202209271c0e55ed5931e343d065f0c12b5
Robin Pindel <robin.pindel@gmail.com>
2023-03-28 21:36:45 -0500

    Initial framework for mod 3 assignment

---
+++ module 3/PindelDedupArrayList.java
@@ -0,0 +1,19 @@
+/*Robin Pindel
+ * Module 3 ArrayList Dedup
+ * 3/28/2023
+ * 
+ * This program will generate an ArrayList of random ints and then dedup that 
+ * ArrayList into a new ArrayList
+ */
+
+import java.util.*;
+
+public class PindelDedupArrayList {
+    // Main method
+    public static void main(String[] args) {
+
+    }
+    public static <E> ArrayList<E> removeDuplicates(ArrayList<E> list) {
+
+    }
+} 
