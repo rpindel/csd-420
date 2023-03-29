01caad6708faa4d514820c2e6f12177e7cfa4541
Robin Pindel <robin.pindel@gmail.com>
2023-03-28 22:15:22 -0500

    Good code work for mod 3 assignment

--- module 3/PindelDedupArrayList.java
+++ module 3/PindelDedupArrayList.java
@@ -11,9 +11,39 @@ import java.util.*;
 public class PindelDedupArrayList {
     // Main method
     public static void main(String[] args) {
+        // Create ArrayList
+        ArrayList<Integer> arrayDup = new ArrayList<>();
 
+        // Populate ArrayList with 50 random values from 1-20
+        for (int i = 0; i < 50; i++) {
+            arrayDup.add((int)(Math.random() * 20));
+        }
+
+        // Print original ArrayList to console
+        System.out.println("");
+        System.out.println("The original ArrayList is: ");
+        arrayDup.forEach(e -> System.out.print(e + " "));
+
+        //Create new ArrayList and populate it with dedup'd values from 
+        //original ArrayList
+        ArrayList<Integer> arrayDedup = removeDuplicates(arrayDup);
+
+        // Print new ArrayList
+        System.out.println("");
+        System.out.println("\nThe dedup'd ArrayList is: ");
+        arrayDedup.forEach(e -> System.out.print(e + " "));
+        System.out.println("");
+        System.out.println("");
     }
     public static <E> ArrayList<E> removeDuplicates(ArrayList<E> list) {
+        // Create new ArrayList        
+        ArrayList<E> arrayDedup = new ArrayList<>();
 
+        for (E element : list) {
+            if (!arrayDedup.contains(element)) {
+                arrayDedup.add(element);
+            }
+        }
+        return arrayDedup;
     }
 } 
