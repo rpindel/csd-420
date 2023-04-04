d4ebeb27a214c826858f639bb0a403acac3ba217
Robin Pindel <robin.pindel@gmail.com>
2023-04-03 19:47:04 -0500

    db post mod 5 code

---
+++ module 5/DBPostCode.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class DBPostCode {
+    public static void main(String[] args) {
+        Map<String, String> pokedexInsertion = new LinkedHashMap<>();
+        pokedexInsertion.put("001", "Bulbasaur");
+        pokedexInsertion.put("002", "Ivysaur");
+        pokedexInsertion.put("003", "Venusaur");
+        System.out.println("LinkedHashMap pokedex in Insertion Order: ");
+        System.out.println(pokedexInsertion + "\n");
+
+        Map<String, String> pokedexAccess = new LinkedHashMap<>(6, 0.75f, true);
+        pokedexAccess.put("001", "Bulbasaur");
+        pokedexAccess.put("002", "Ivysaur");
+        pokedexAccess.put("003", "Venusaur");
+        System.out.println("LinkedHashMap pokedex in Access order before any entries accessed: ");
+        System.out.println(pokedexAccess + "\n");
+        System.out.println("003 = " + pokedexAccess.get("003"));
+        System.out.println("002 = " + pokedexAccess.get("002"));
+        System.out.println("001 = " + pokedexAccess.get("001"));
+        System.out.println("LinkedHashMap pokedex in Access order after entries accessed: ");
+        System.out.println(pokedexAccess + "\n");
+    }
+}
