2f11a5f85d56ed80f01d96c962ce6f02549fbcdf
Robin Pindel <robin.pindel@gmail.com>
2023-04-08 15:43:32 -0500

    Final code for mod5 assignment

--- mod5/PindelWordsTreeSet.java
+++ mod5/PindelWordsTreeSet.java
@@ -3,21 +3,19 @@
  * 4/6/2023
  * 
  * This program ingests a text file containing words, adds unique words to a 
- * TreeSet, and then outputs the list of unique words in ascending and 
- * descending order */
+ * HashSet and TreeSet, and then outputs the list of unique words in no, ascending,
+ * and descending order 
+ */
 
 import java.util.*;
 import java.io.*;
-import java.net.*;
 
 public class PindelWordsTreeSet {
     // Main method
-    public static void main(String[] args) throws Exception {        
-        //PrintWriter putTest = new PrintWriter("putTest.txt");
-        
-        // Ingest/read text file with words
+    public static void main(String[] args) throws Exception {                
+        // Ingest/read text file with words and create a HashSet
         // String file1 = "C:\\Users\\tk78876\\csd\\csd-420\\mod5\\collection_of_words.txt";
-        String file1 = "collection_of_words.txt"; 
+        String file1 = "./collection_of_words.txt"; 
         File wordFile1 = new File(file1);
         Scanner wordsIngress1 = new Scanner(wordFile1);
         Set<String> hashSet = new HashSet<>();
@@ -25,8 +23,9 @@ public class PindelWordsTreeSet {
             hashSet.add(wordsIngress1.next());
         }
 
+        // Ingest/read text file with words and create a TreeSet
         // String file2 = "C:\\Users\\tk78876\\csd\\csd-420\\mod5\\collection_of_words.txt";
-        String file2 = "collection_of_words.txt";
+        String file2 = "./collection_of_words.txt";
         File wordFile2 = new File(file2);
         Scanner wordsIngress2 = new Scanner(wordFile2);
         NavigableSet<String> treeSet = new TreeSet<>();
@@ -38,18 +37,18 @@ public class PindelWordsTreeSet {
         wordsIngress2.close();
 
         // Output Hashset in standard (non-ordered) order
-        System.out.println("\nThe collection_of-words.txt file contains the " + 
+        System.out.println("\nThe collection_of_words.txt file contains these " + 
         "words, in no particular order: ");
         System.out.println(hashSet);
         
         // Output TreeSet in ascending order
-        System.out.println("\nThe collection_of-words.txt file contains the " + 
+        System.out.println("\nThe collection_of_words.txt file contains these " + 
         "words, in ascending order: ");
         System.out.println(treeSet);
 
         // Create reverse order TreeSet and output
         NavigableSet<String> revTreeSet = treeSet.descendingSet();
-        System.out.println("\nThe collection_of-words.txt file contains the " + 
+        System.out.println("\nThe collection_of_words.txt file contains these " + 
         "words, in descending order: ");
         System.out.println(revTreeSet);
         System.out.println("");
