b5c55b075f02a6116cc4112bd2bd6062d7ed2c93
Pindel,Robin <Robin.Pindel@kohls.com>
2023-04-13 12:09:18 -0500

    Code pushes

--- mod6/DBPostCode.java
+++ mod6/DBPostCode.java
@@ -1,13 +1,14 @@
 public class DBPostCode {
+    
+    enum Effectiveness {
+        RESISTED,
+        NEUTRAL,
+        SUPER_EFFECTIVE
+    }
     public static void main(String[] args) {
-        enum Effectiveness {
-            RESISTED,
-            NEUTRAL,
-            SUPER_EFFECTIVE
-        }
-    Effectiveness fireAgainstWaterAttack = Effectiveness.RESISTED;
-    System.out.println("");
-    System.out.println(fireAgainstWaterAttack);
-    System.out.println("");
+        Effectiveness fireAgainstWaterAttack = Effectiveness.RESISTED;
+        System.out.println("");
+        System.out.println(fireAgainstWaterAttack);
+        System.out.println("");
     }
 }
---
+++ mod6/PindelBubbleSortGeneric.java
@@ -0,0 +1,25 @@
+/* Robin pindel
+ * Module 6 Bubble Sort Programming Assignment
+ * 4/13/2023
+ * 
+ * This problem ... <does what?>
+ */
+
+import java.util.*;
+
+public class PindelBubbleSortGeneric {
+    // Main method
+    public static void main(String[] args) {
+
+    }
+    
+    // Comparable interface method
+    public static <E extends Comparable<E>> void bubbleSort(E[] list) {
+
+    }
+
+    // Comaparator interface method
+    public static <E> void bubbleSort(E[] list, Comparator<? super E> comparator) {
+
+    }
+}
