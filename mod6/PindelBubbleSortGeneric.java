5ff3be284b128168aa8cdffba9de12b77b1440e0
Pindel,Robin <Robin.Pindel@kohls.com>
2023-04-13 12:11:15 -0500

    Code tweaks for mod6 assignment

--- mod6/PindelBubbleSortGeneric.java
+++ mod6/PindelBubbleSortGeneric.java
@@ -14,11 +14,13 @@ public class PindelBubbleSortGeneric {
     }
     
     // Comparable interface method
+    // Sort data using this interface - Pokedex numbers in an array?
     public static <E extends Comparable<E>> void bubbleSort(E[] list) {
 
     }
 
     // Comaparator interface method
+    // Sort data using this interface - Pokemon names in an array?
     public static <E> void bubbleSort(E[] list, Comparator<? super E> comparator) {
 
     }
