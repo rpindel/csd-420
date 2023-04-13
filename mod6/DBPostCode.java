6bfcec8324019d1146c65966ad68edd908b84deb
Robin Pindel <robin.pindel@gmail.com>
2023-04-12 21:29:20 -0500

    mod6 db post code

---
+++ mod6/DBPostCode.java
@@ -0,0 +1,13 @@
+public class DBPostCode {
+    public static void main(String[] args) {
+        enum Effectiveness {
+            RESISTED,
+            NEUTRAL,
+            SUPER_EFFECTIVE
+        }
+    Effectiveness fireAgainstWaterAttack = Effectiveness.RESISTED;
+    System.out.println("");
+    System.out.println(fireAgainstWaterAttack);
+    System.out.println("");
+    }
+}
