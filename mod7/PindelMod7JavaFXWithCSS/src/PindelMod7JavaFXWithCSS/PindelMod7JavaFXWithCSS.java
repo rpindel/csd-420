1dbc6cce8c44d71ea7bb7cf4dc6c03291774428d
Robin Pindel <robin.pindel@gmail.com>
2023-04-19 21:10:06 -0500

    Code tweaks

--- mod7/PindelMod7JavaFXWithCSS/src/PindelMod7JavaFXWithCSS/PindelMod7JavaFXWithCSS.java
+++ mod7/PindelMod7JavaFXWithCSS/src/PindelMod7JavaFXWithCSS/PindelMod7JavaFXWithCSS.java
@@ -6,12 +6,41 @@ import javafx.scene.*;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
+import javafx.scene.shape.*;
 
 public class PindelMod7JavaFXWithCSS extends Application {
     //Override the start method of the Application class
     @Override
-    public static start(Stage primaryStage) {
+    public void start(Stage primaryStage) {
+        HBox hBox = new HBox();
+        Scene scene = new Scene(hBox, 400, 100);
+        scene.getStylesheets().add("PindelMod7JavaFXWithCSS.css");
+        
+        StackPane stackPane1 = new StackPane();
+        stackPane1.getStyleClass().add("border");
+        StackPane stackPane2 = new StackPane();
+        StackPane stackPane3 = new StackPane();
+        StackPane stackPane4 = new StackPane();
 
+        Circle circle1 = new Circle(50);
+        circle1.getStyleClass().add("plaincircle");
+        Circle circle2 = new Circle(50);
+        circle2.getStyleClass().add("plaincircle");
+        Circle circle3 = new Circle(50);
+        circle3.setId("redcircle");
+        Circle circle4 = new Circle(50);
+        circle4.setId("greencircle");
+
+        stackPane1.getChildren().add(circle1);
+        stackPane2.getChildren().add(circle2);
+        stackPane3.getChildren().add(circle3);
+        stackPane4.getChildren().add(circle4);
+
+        hBox.getChildren().addAll(stackPane1, stackPane2, stackPane3, stackPane4);
+
+        primaryStage.setTitle("Circles with JavaFX CSS");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
     //Main method since I am using VSCode
     public static void main(String[] args) {
