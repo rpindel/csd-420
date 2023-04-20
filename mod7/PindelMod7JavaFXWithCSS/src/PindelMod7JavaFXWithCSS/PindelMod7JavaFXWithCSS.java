2600ab919c424a06202e3ce3bb705e9e880f3938
Robin Pindel <robin.pindel@gmail.com>
2023-04-19 20:45:59 -0500

    Mod 7 code

--- mod7/PindelMod7JavaFXWithCSS/src/PindelMod7JavaFXWithCSS/PindelMod7JavaFXWithCSS.java
+++ mod7/PindelMod7JavaFXWithCSS/src/PindelMod7JavaFXWithCSS/PindelMod7JavaFXWithCSS.java
@@ -1,5 +1,20 @@
 package PindelMod7JavaFXWithCSS;
 
-public class PindelMod7JavaFXWithCSS {
-    
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+public class PindelMod7JavaFXWithCSS extends Application {
+    //Override the start method of the Application class
+    @Override
+    public static start(Stage primaryStage) {
+
+    }
+    //Main method since I am using VSCode
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
 }
