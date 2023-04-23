2fa47accf16231bb2c1c1cc0ac8e61d8e49c2602
Robin Pindel <robin.pindel@gmail.com>
2023-04-23 09:05:27 -0500

    Final code for mod7 assignment

--- mod7/PindelMod7JavaFXWithCSS/src/PindelMod7JavaFXWithCSS/PindelMod7JavaFXWithCSS.java
+++ mod7/PindelMod7JavaFXWithCSS/src/PindelMod7JavaFXWithCSS/PindelMod7JavaFXWithCSS.java
@@ -1,3 +1,10 @@
+/* Robin Pindel
+ * Module 7 Programming Assignment
+ * 4/23/2023
+
+ * This program displays four JavaFX circles that are styled using a CSS stylesheet.
+ */
+
 package PindelMod7JavaFXWithCSS;
 
 import javafx.application.Application;
@@ -11,19 +18,20 @@ public class PindelMod7JavaFXWithCSS extends Application {
     //Override the start method of the Application class
     @Override
     public void start(Stage primaryStage) {
+        // Create general containers to hold nodes
         HBox hBox = new HBox(5);
         hBox.setPadding(new Insets(5));
         Scene scene = new Scene(hBox, 450, 450);
         scene.getStylesheets().add("PindelMod7JavaFXWithCSS.css");
-        //System.out.println(getClass().getResource("\\rsc\\PindelMod7JavaFXWithCSS.css").toExternalForm());
 
-        
+        // Create stackpane containers to hold individual circles
         StackPane stackPane1 = new StackPane();
         stackPane1.getStyleClass().add("border");
         StackPane stackPane2 = new StackPane();
         StackPane stackPane3 = new StackPane();
         StackPane stackPane4 = new StackPane();
 
+        // Create individual circles and style them via CSS
         Circle circle1 = new Circle(50);
         circle1.getStyleClass().add("plaincircle");
         Circle circle2 = new Circle(50);
@@ -33,13 +41,16 @@ public class PindelMod7JavaFXWithCSS extends Application {
         Circle circle4 = new Circle(50);
         circle4.setId("greencircle");
 
+        // Add circles to their stackpane
         stackPane1.getChildren().add(circle1);
         stackPane2.getChildren().add(circle2);
         stackPane3.getChildren().add(circle3);
         stackPane4.getChildren().add(circle4);
 
+        // Add all stackpanes to main container hbox
         hBox.getChildren().addAll(stackPane1, stackPane2, stackPane3, stackPane4);
 
+        // Set the scene and display the stage
         primaryStage.setTitle("Circles with JavaFX CSS");
         primaryStage.setScene(scene);
         primaryStage.show();
